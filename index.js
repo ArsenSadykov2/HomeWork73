40fd91ed2d81776aef3fca669c0f1c563fd6301e
Sadykov Arsen <LoCo10291998@gmail.com>
2025-03-14 17:20:27 +0600

    Commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
 const port = 8000;
+const task2 = Vigenere();
+const password = 'apple';
 
 
 app.get('/', (req, res) => {
@@ -16,7 +19,27 @@ app.get('*', (req, res) => {
     return res.status(404).send('Not Found');
 });
 
+task2.get('/encode/:text', (req, res) => {
+    const text = req.params.text;
+    const encode = Vigenere.Cipher(password).crypt(text);
+    return res.send(encode);
+})
+
+task2.get('/decode/:text', (req, res) => {
+    const text = req.params.text;
+    const decode = Vigenere.Decipher(password).crypt(text);
+    return res.send(decode);
+});
+
+task2.get('*', (req, res) => {
+    return res.status(404).send('Not Found');
+});
+
 
 app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
+task2.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
