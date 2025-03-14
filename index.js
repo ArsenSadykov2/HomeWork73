2a67fc1bddf58e7c2233b600babb9d8adc1877f3
Sadykov Arsen <LoCo10291998@gmail.com>
2025-03-14 17:38:40 +0600

    Task 2 is done. Made changing text with Vigenere crypt.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
 const port = 8000;
-const task2 = Vigenere();
 const password = 'apple';
 
 
@@ -15,31 +14,23 @@ app.get('/get/:id', (req, res) => {
     return res.send(`${req.params.id}`);
 });
 
-app.get('*', (req, res) => {
-    return res.status(404).send('Not Found');
-});
-
-task2.get('/encode/:text', (req, res) => {
+app.get('/encode/:text', (req, res) => {
     const text = req.params.text;
     const encode = Vigenere.Cipher(password).crypt(text);
     return res.send(encode);
-})
+});
 
-task2.get('/decode/:text', (req, res) => {
+app.get('/decode/:text', (req, res) => {
     const text = req.params.text;
     const decode = Vigenere.Decipher(password).crypt(text);
     return res.send(decode);
 });
 
-task2.get('*', (req, res) => {
+app.get('*', (req, res) => {
     return res.status(404).send('Not Found');
 });
 
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
-
-task2.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
